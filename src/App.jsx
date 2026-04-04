99eb578c8305a3bd48cdfb5d77e7237127e1b22a
sathish6502 <madevsathishkumar6502@gmail.com>
2026-04-04 15:14:35 +0530

    Header and Home commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./Components/Header/Header";
+import Home from "./Pages/Home"
+
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
-  )
-}
+    <BrowserRouter>
 
-export default App
+      <Header />
+
+      <Routes>
+           <Route path="/" element={<Home />} /> 
+
+      </Routes>
+
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Components/Header/Header";
+import Hero from "../Components/Home/Hero";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+};
+
+export default Home;

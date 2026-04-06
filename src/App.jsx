af61033192fa0606a9763e1c2717ff5b4ceec11f
rajaduraicbe <rajadurai2496cbe@gmail.com>
2026-04-06 17:13:59 +0530

    last changes commit in the form section two files deletd

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ const App = () => {
     <div>
       <Getintouch />
       <Form />
-      <ContactInfoCards />
-      <Startegy />
+      {/* <ContactInfoCards /> */}
+      {/* <Startegy /> */}
        < TestimonialSection/>
    </div>
   )

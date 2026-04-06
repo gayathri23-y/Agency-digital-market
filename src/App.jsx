0d7c9fa4bf98c4b629e4377cbdb5b7d5feaa2c9c
rajaduraicbe <rajadurai2496cbe@gmail.com>
2026-04-06 17:26:43 +0530

    slight change in appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import Getintouch from './Components/Contact/Getintouch'
 import TestimonialSection from './Pages/Testimonal';
 import Form from './Components/Contact/Form';
-import ContactInfoCards from './Components/Contact/ContactInfoCards';
-import Startegy from "./Components/Contact/Startegy";
+
 
 const App = () => {
   return (

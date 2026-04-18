200751713013cc80b1116896e7107d79de8d0dcc
rajaduraicbe <rajadurai2496cbe@gmail.com>
2026-04-18 09:40:57 +0530

    commit

---
+++ src/Components/Contact/Getintouch.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Getintouch = () => {
+  return (
+     <section className="w-full min-h-[400px] bg-white flex items-center justify-center px-4 sm:px-6 md:px-8 lg:px-16 xl:px-16">
+      <div className="w-full max-w-5xl text-center px-4 sm:px-6 md:px-8 lg:px-16 xl:px-16">
+        <button className="mb-8 rounded-lg border border-gray-300 bg-white px-3 py-1 text-xs font-medium text-black shadow-sm">
+          Get In Touch
+        </button>
+
+        <h1 className="text-3xl sm:text-3xl md:text-3xl lg:text-5xl font-normal tracking-tight text-black mb-6">
+          Ready to Grow Your Business?
+        </h1>
+
+        <p className="text-base sm:text-lg md:text-xl lg:text-xl leading-relaxed text-gray-500 max-w-4xl mx-auto">
+          Let&apos;s discuss how we can help you achieve your digital marketing goals. Get a free <br/> 
+         consultation and custom strategy proposal.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Getintouch
